c4a273f7215e6a51d1222a7867cdb7d2a58e29c3
MadHatter <ivkalgin@gmail.com>
2018-07-16 17:15:56 +0700

    added 'not found' handler

--- backend/controllers/ControllersFacade.js
+++ backend/controllers/ControllersFacade.js
@@ -1,18 +1,27 @@
 const Express = require('express');
-
-const SchemasController = require('./SchemasController');
 const DocumentsController = require('./DocumentsController');
+const LoggerFactory = require('../utils/log');
+const SchemasController = require('./SchemasController');
 
-class ControllersFacsde {
+class ControllersFacade {
     constructor(services) {
 	this.schemasController = new SchemasController(services);
 	this.documentsController = new DocumentsController(services);
+	this._logger = LoggerFactory('ControllersFacsde');
     }
 
     createRouter() {
 	const router = new Express();
 	router.use('/schemas', this.schemasController.createRouter());
 	router.use('/documents', this.documentsController.createRouter());
+	router.use(this._notFoundHandler)
 	return router;
+    }
+
+    _notFoundHandler(request, respones, next) {
+	respones.status(HttpStatusCodes.NOT_FOUND);
+	respones.json({error: 'Not found'});
+	this._logger.warn('%s %d %s', request.method, respones.statusCode, request.url);
+	respones.end();
     }
 }
