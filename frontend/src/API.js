75d24f910e38eb3c76e822a1c87605fa3009dcfd
dmitry <dmitry.zyryanov@execution.su>
2018-07-31 09:48:37 +0700

    test schemas extracted to json-s

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,5 +1,7 @@
 import request from 'request-promise';
 import config from './config';
+import testIdPart from './test-id-part';
+import testDataPart from './test-data-part';
 
 const API = {
     _getBaseUrl() {
@@ -9,99 +11,8 @@ const API = {
     getSchema(schemaId) {
         if (schemaId === 't') {
             return Promise.resolve({
-                idPart: {
-                    definitions: {
-                        TIN: {
-                            type: 'string',
-                            default: '00000000'
-                        },
-                        Sum: {
-                            type: 'number',
-                            default: 0
-                        }
-                    },
-                    "title": "Акт",
-                    "type": "object",
-                    "required": [],
-                    "properties": {
-                        "date": {
-                            "type": "string",
-                            "title": "Дата"
-                        },
-                        "number": {
-                            "type": "string",
-                            "title": "Номер"
-                        },
-                        "producer": {
-                            "$ref": "#/definitions/TIN",
-                            "title": "Поставщик"
-                        },
-                        "consumer": {
-                            "$ref": "#/definitions/TIN",
-                            "title": "Покупатель"
-                        },
-                        "desc": {
-                            "type": "string",
-                            "title": "Краткое описание услуг"
-                        },
-                        "sumWOVAT": {
-                            "$ref": "#/definitions/Sum",
-                            "title": "Сумма Акта без НДС"
-                        },
-                        "VAT": {
-                            "$ref": "#/definitions/Sum",
-                            "title": "Сумма НДС"
-                        },
-                        "sumWVAT": {
-                            "$ref": "#/definitions/Sum",
-                            "title": "Сумма с НДС"
-                        }
-                    },
-                },
-                dataPart: {
-                    definitions: {
-                        Operation: {
-                            "type": "object",
-                            "properties": {
-                                "postingDate": {
-                                    "type": "string",
-                                    "title": "Дата списывания"
-                                },
-                                "transactionDate": {
-                                    "type": "string",
-                                    "title": "Дата операции"
-                                },
-                                "transactionDescription": {
-                                    "type": "string",
-                                    "title": "Описание операции"
-                                },
-                                "transactionReferenceNumber": {
-                                    "type": "string",
-                                    "title": "Номер транзакции"
-                                },
-                                "amountTransaction": {
-                                    "type": "integer",
-                                    "title": "Сумма в валюте операции"
-                                },
-                                "amountAccount": {
-                                    "type": "integer",
-                                    "title": "Сумма в валюте счёта"
-                                },
-                            }
-                        },
-                    },
-                    "title": "Акт",
-                    type: 'object',
-                    properties: {
-                        "operations": {
-                            "type": "array",
-                            "title": "Операции",
-                            "items": {
-                                "$ref": "#/definitions/Operation"
-                            }
-                        },
-                    }
-                },
+                idPart: testIdPart,
+                dataPart: testDataPart,
             });
         }
         const params = {
