0747c66c93fd576945e65334598c36791af6042d
dmitry <dmitry.zyryanov@execution.su>
2018-08-02 15:15:41 +0700

    remove test schema

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -25,29 +25,6 @@ const API = {
                     data.dataPart.jsonSchema &&
                     data.dataPart.uiSchema)
                 {
-                    data.dynamicPart = {
-                        "jsonSchema": {
-                            "title": "Подтверждение",
-                            "type": "object",
-                            "properties": {
-                                "date": {
-                                    "type": "string",
-                                    "title": "Дата"
-                                },
-                            }
-                        },
-                        "uiSchema": {
-                            "date": {
-                                "ui:widget": "alt-date",
-                                "ui:options": {
-                                    "yearsRange": [
-                                        1980,
-                                        2030
-                                    ]
-                                }
-                            },
-                        }
-                    };
                     return data;
                 }
                 throw new Error('Malformed schemas');
