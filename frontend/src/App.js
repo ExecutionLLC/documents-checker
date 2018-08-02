0a539a250a97db319679effb403718f9da082017
dmitry <dmitry.zyryanov@execution.su>
2018-08-02 12:42:34 +0700

    document confirmation page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AddDocument from './AddDocument/AddDocument';
 import CheckDocument from './CheckDocument/CheckDocument';
+import DocumentConfirmation from './DocumentConfirmation/DocumentConfirmation';
 
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
             <Switch>
                 <Route path="/adddoc" component={AddDocument} />
                 <Route path="/checkdoc" component={CheckDocument} />
+                <Route path="/confirmation" component={DocumentConfirmation} />
             </Switch>
         </Router>
     );
---
+++ frontend/src/DocumentConfirmation/DocumentConfirmation.jsx
@@ -0,0 +1,190 @@
+import React, { Component } from 'react';
+import Form from 'react-jsonschema-form';
+import { PageHeader, Panel, ProgressBar } from 'react-bootstrap';
+import ErrorPanel from '../Components/ErrorPanel';
+import Navigation from '../Components/Navigation';
+import API from '../API';
+import config from '../config';
+
+
+class DocumentConfirmation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            schema: {
+                data: null,
+                error: null,
+            },
+            check: {
+                isExists: null,
+                data: null,
+                isLoading: false,
+                error: null,
+            },
+            formsData: {
+                idPart: null,
+            }
+        };
+    }
+
+    componentDidMount() {
+        API.getSchema(config.SCHEMA_ID)
+            .then((data) => {
+                this.setState({
+                    ...this.state,
+                    schema: {
+                        data,
+                        error: null,
+                    }
+                });
+            })
+            .catch((error) => {
+                this.setState({
+                    ...this.state,
+                    schema: {
+                        data: null,
+                        error,
+                    }
+                });
+            });
+    }
+
+    onDocumentIdSubmit(documentIdPart) {
+        if (!this.state.schema.data) {
+            return;
+        }
+        this.setState({
+            check: {
+                ...this.state.check,
+                isExists: null,
+                data: null,
+                isLoading: true,
+                error: null,
+            },
+            formsData: {
+                idPart: documentIdPart,
+            }
+        });
+        API.isDocumentExists(config.SCHEMA_ID, documentIdPart)
+            .then(isExists => {
+                this.setState({
+                    check: {
+                        ...this.state.check,
+                        isExists,
+                        data: null,
+                        isLoading: !isExists,
+                        error: null,
+                    }
+                });
+                return isExists;
+            })
+            .then((isExist) => {
+                if (!isExist) {
+                    return null;
+                }
+                return API.getDocument(config.SCHEMA_ID, documentIdPart);
+            })
+            .then((data) => {
+                this.setState({
+                    check: {
+                        ...this.state.check,
+                        data,
+                        isLoading: false,
+                        error: null,
+                    }
+                });
+            })
+            .catch((error) => {
+                this.setState({
+                    check: {
+                        ...this.state.check,
+                        isLoading: false,
+                        error,
+                    }
+                });
+            });
+    }
+
+    renderSchemaError() {
+        return (
+            <ErrorPanel
+                title="Schema loading error"
+                content={`${this.state.schema.error}`}
+            />
+        );
+    }
+
+    renderDocumentForm() {
+        const schemaData = this.state.schema.data;
+        return (
+            <div>
+                {schemaData && (
+                    <Form
+                        schema={schemaData.idPart.jsonSchema}
+                        uiSchema={schemaData.idPart.uiSchema}
+                        formData={this.state.formsData.idPart}
+                        onSubmit={({formData}) => this.onDocumentIdSubmit(formData)}
+                        onError={(errors) => console.log("Errors: ",  errors)}
+                    />
+                )}
+            </div>
+        );
+    }
+
+    renderDocumentDoesNotExist() {
+        return (
+            <Panel bsStyle="warning">
+                <Panel.Heading>
+                    <Panel.Title componentClass="h3">
+                        Document
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body>
+                    Does not exists
+                </Panel.Body>
+            </Panel>
+        );
+    }
+
+    renderDocument() {
+        const schemaData = this.state.schema.data;
+        return (
+            <Panel bsStyle="info">
+                <Panel.Heading>
+                    <Panel.Title componentClass="h3">
+                        Document
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body>
+                    <Form
+                        schema={schemaData.dataPart.jsonSchema}
+                        uiSchema={{...schemaData.dataPart.uiSchema, 'ui:readonly': true}}
+                        formData={this.state.check.data.dataPart}
+                    >
+                        <button type="submit" style={{display: 'none'}} />
+                    </Form>
+                </Panel.Body>
+            </Panel>
+        );
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                <Navigation
+                    page={this.props.match.url}
+                />
+                <PageHeader>
+                    Document confirmation
+                </PageHeader>
+                {this.state.schema.error ?
+                    this.renderSchemaError() :
+                    this.renderDocumentForm()
+                }
+            </div>
+        );
+    }
+}
+
+export default DocumentConfirmation;
