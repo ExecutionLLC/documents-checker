8076ad94955a91c5914606b47dc76ff1b0b5448f
dmitry <dmitry.zyryanov@execution.su>
2018-08-02 17:36:50 +0700

    fix for form validation reset on submit, using in add document

--- frontend/src/Components/FormStateSafe.jsx
+++ frontend/src/Components/FormStateSafe.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Form from "react-jsonschema-form";
 
 // Default Form fails to reset validation whet onSubmit calls setState
-function FormStateSafe(props) {
-    return (
-        <Form
-            {...props}
-            onSubmit={(...args) =>
-                setTimeout(
-                    () => props.onSubmit(...args),
-                    0
-                )
-            }
-        />
-    );
+// Can not be stateless due to using its ref
+class FormStateSafe extends Component {
+    constructor(props) {
+        super(props);
+        this.formComponent = null;
+    }
+
+    onSubmit(evt) {
+        if (this.formComponent) {
+            this.formComponent.onSubmit(evt);
+        }
+    }
+
+    render() {
+        return (
+            <Form
+                {...this.props}
+                onSubmit={(...args) =>
+                    setTimeout(
+                        () => this.props.onSubmit(...args),
+                        0
+                    )
+                }
+                ref={(comp) => {this.formComponent = comp;}}
+            />
+        );
+    }
 }
 
 export default FormStateSafe;
