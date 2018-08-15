b576427adcdf954285f6cdf18c7429bcff06343d
andrey.ableev <andre.ableev@execution.su>
2018-08-15 19:19:54 +0700

    implement compare documents feature

--- frontend/src/CompareDocuments/CompareDocuments.jsx
+++ frontend/src/CompareDocuments/CompareDocuments.jsx
@@ -36,7 +36,27 @@ class CompareDocuments extends Component {
         idPart: null,
       }
     };
-    console.log(this.state.schema.data);
+
+    // console.log(`23:30, 03:00 :`, CleaningOfStreets.durationHours('23:30', '03:00'));
+    // console.log(`23:30, 23:31 :`, CleaningOfStreets.durationHours('23:30', '23:31'));
+    // console.log(`00:00, 00:00 :`, CleaningOfStreets.durationHours('00:00', '00:00'));
+    //
+    //
+    // console.log(' ----------- dayDurationHours ----------- ');
+    // console.log(`23:30, 08:30 :`, CleaningOfStreets.dayDurationHours('23:30', '08:30'));
+    // console.log(`23:30, 03:00 :`, CleaningOfStreets.dayDurationHours('23:30', '03:00'));
+    // console.log(`07:00, 23:00 :`, CleaningOfStreets.dayDurationHours('07:00', '23:00'));
+    // console.log(`00:00, 00:00 :`, CleaningOfStreets.dayDurationHours('00:00', '00:00'));
+    // console.log(`10:00, 11:30 :`, CleaningOfStreets.dayDurationHours('10:00', '11:30'));
+    //
+    //
+    // console.log(' ----------- nightDurationHours ----------- ');
+    // console.log(`23:30, 08:30 :`, CleaningOfStreets.nightDurationHours('23:30', '08:30'));
+    // console.log(`23:30, 03:00 :`, CleaningOfStreets.nightDurationHours('23:30', '03:00'));
+    // console.log(`07:00, 23:00 :`, CleaningOfStreets.nightDurationHours('07:00', '23:00'));
+    // console.log(`00:00, 00:00 :`, CleaningOfStreets.nightDurationHours('00:00', '00:00'));
+    // console.log(`23:00, 07:00 :`, CleaningOfStreets.nightDurationHours('23:00', '07:00'));
+    // console.log(`10:00, 11:30 :`, CleaningOfStreets.nightDurationHours('10:00', '11:30'));
   }
 
   compareDocuments() {
@@ -244,7 +264,11 @@ class CompareDocuments extends Component {
         schema={this.state.schema.data.dataPart.jsonSchema}
         uiSchema={readOnlyUiSchema}
         formData={data}
-      />
+      >
+        <div>
+          <button type="submit" hidden>Submit</button>
+        </div>
+      </Form>
     );
   }
 
--- frontend/src/validations/cleaningOfStreets.js
+++ frontend/src/validations/cleaningOfStreets.js
@@ -1,28 +1,119 @@
+import moment from 'moment';
 import config from '../config';
 
 class CleaningOfStreets {
-  static compare(routingSheetJob, reportJob, stats) {
+  static durationHoursMoment(momentBeg, momentEnd) {
+    const diff = moment.duration(momentEnd.diff(momentBeg));
+    return diff.hours() + (diff.minutes() / 60);
+  }
 
-    // fill stats
-    stats.materialsTotal += reportJob.result;
+  static durationHours(hhmm1, hhmm2) {
+    const beg = moment(`2018-01-01 ${hhmm1}`);
+    let end = moment(`2018-01-01 ${hhmm2}`);
+    if (beg > end) {
+      return this.durationHours(hhmm1, '24:00')
+        + this.durationHours('00:00', hhmm2);
+      // end = moment(`2018-01-02 ${hhmm2}`);
+    }
+    return this.durationHoursMoment(beg, end);
+    // const diff = moment.duration(end.diff(beg));
+    // return diff.hours() + (diff.minutes() / 60);
+  }
+
+  static nightDurationHours(hhmm1, hhmm2) {
+    return this.durationHours(hhmm1, hhmm2) - this.dayDurationHours(hhmm1, hhmm2);
+  }
+
+  static dayDurationHours(hhmm1, hhmm2) {
+
+    const calcIn24Hours = (moment1, moment2) => {
+      const dayTimeBeg = moment1.clone().hour(config.DAY_TIME_BEG).minute(0);
+      const dayTimeEnd = moment1.clone().hour(config.DAY_TIME_END).minute(0);
+
+      if (moment1 > dayTimeEnd || moment2 < dayTimeBeg) { // интвервал снаружи интервала дневного времени
+        return 0;
+      } else if (moment1 <= dayTimeBeg && moment2 >= dayTimeEnd) { // интвервал покрывает весь интервал дневного времени
+        return this.durationHoursMoment(dayTimeBeg, dayTimeEnd);
+      } else if (moment1 > dayTimeBeg && moment2 < dayTimeEnd) { // интервал находится внутри дневного времени
+        return this.durationHoursMoment(moment1, moment2);
+      } else if (moment1 < dayTimeBeg && moment2 < dayTimeEnd) {
+        return this.durationHoursMoment(dayTimeBeg, moment2);
+      } else if (moment1 > dayTimeBeg && moment2 > dayTimeEnd) {
+        return this.durationHoursMoment(moment1, dayTimeEnd);
+      }
+    };
+
+    const beg = moment(`2018-01-01 ${hhmm1}`);
+    const end = moment(`2018-01-01 ${hhmm2}`);
+    if (beg > end) {
+      const beg1 = moment(`2018-01-01 ${hhmm1}`);
+      const end1 = moment(`2018-01-01 24:00`);
+
+      const beg2 = moment(`2018-01-02 00:00`);
+      const end2 = moment(`2018-01-02 ${hhmm2}`);
+
+      // console.log(`[${beg1.toString()}, ${end1.toString()}] and [${beg2.toString()}, ${end2.toString()}]`);
+      // console.log(calcIn24Hours(beg1, end1), calcIn24Hours(beg2, end2));
+
+      return calcIn24Hours(beg1, end1) +
+        calcIn24Hours(beg2, end2);
+    } else {
+      return calcIn24Hours(beg, end);
+    }
+  }
+
+  static processJobItem(routingSheetJob, reportJob, stats) {
+
+    let approved = true;
+
 
     if (routingSheetJob.geozone !== reportJob.geozone) {
       console.log(routingSheetJob.geozone, reportJob.geozone);
-      return false;
+      approved = false;
     }
 
-    const hourDiffRatio = Math.abs((reportJob.totalTime - routingSheetJob.totalTime) / routingSheetJob.totalTime);
+    const routingSheetJobTotalTime =
+      CleaningOfStreets.durationHours(routingSheetJob.startTime, routingSheetJob.endTime);
+
+    const hourDiffRatio = Math.abs((reportJob.totalTime - routingSheetJobTotalTime) / routingSheetJobTotalTime);
     if (hourDiffRatio > config.ALLOWED_TIME_DELTA) {
       console.log('hourDiffRatio: ', hourDiffRatio);
-      return false;
+      approved = false;
     }
 
-    return true;
+    // fill stats
+    stats.materials += reportJob.result;
+    stats.approved = stats.approved && approved;
+
+    const reportDayHours = CleaningOfStreets.dayDurationHours(reportJob.startTime, reportJob.endTime);
+    console.log(reportDayHours, reportJob.startTime, reportJob.endTime);
+    const routingSheetDayHours = CleaningOfStreets.dayDurationHours(routingSheetJob.startTime, routingSheetJob.endTime);
+    stats.approvedDayHours += reportDayHours;
+    stats.diffDayHours += reportDayHours - routingSheetDayHours;
+
+    const reportNightHours = CleaningOfStreets.nightDurationHours(reportJob.startTime, reportJob.endTime);
+    const routingSheetNightHours = CleaningOfStreets.nightDurationHours(routingSheetJob.startTime, routingSheetJob.endTime);
+    stats.approvedNightHours += reportNightHours;
+    stats.diffNightHours += reportNightHours - routingSheetNightHours;
+
+    return {
+      geozone: routingSheetJob.geozone,
+      // dayOrNight: "День",
+      hoursByRoutingSheet: routingSheetJobTotalTime,
+      hoursApproved: reportJob.totalTime,
+      approved: approved ? "Да" : "Нет",
+      materials: reportJob.result,
+    };
   }
 
   static verify(routingSheet, report) {
     const stats = {
-      materialsTotal: 0, // вывезено снега на полигон
+      approvedDayHours: 0,
+      diffDayHours: 0,
+      approvedNightHours: 0,
+      diffNightHours: 0,
+      materials: 0, // вывезено снега на полигон
+      approved: true,
     };
     console.log('verifying...');
     // compare job list
@@ -30,22 +121,19 @@ class CleaningOfStreets {
     for (let i = 0; i < routingSheet.jobsList.length; i++) {
       const routingSheetJob = routingSheet.jobsList[i];
       const reportJob = report.jobsList[i];
-
-      const approved = CleaningOfStreets.compare(routingSheetJob, reportJob, stats);
-
-      compareJobsList.push({
-        geozone: routingSheetJob.geozone,
-        // dayOrNight: "День",
-        hoursByRoutingSheet: routingSheetJob.totalTime,
-        hoursApproved: reportJob.totalTime,
-        approved: approved ? "Да" : "Нет",
-        materials: reportJob.result,
-      });
+      compareJobsList.push(CleaningOfStreets.processJobItem(routingSheetJob, reportJob, stats));
+      console.log(stats);
     }
 
     return {
       compareDocHeader: {
-        materials: stats.materialsTotal,
+        jobType: routingSheet.jobsListHeader.jobType,
+        approvedDayHours: stats.approvedDayHours,
+        diffDayHours: stats.diffDayHours,
+        approvedNightHours: stats.approvedNightHours,
+        diffNightHours: stats.diffNightHours,
+        materials: stats.materials,
+        approved: stats.approved ? "Да" : "Нет",
       },
       compareJobsList,
     };
